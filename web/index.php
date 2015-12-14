d577ecb090d5767941190556eaf41f545dc85887
vagrant <vagrant@precise32.(none)>
2015-12-14 21:44:40 +0000

    add site

---
+++ web/index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Ninja Store</title>
+
+  <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+  <link href='assets/css/main.css' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+
+  <div id="main">
+    <div id="header">
+
+      <img id="img-logo" src="images/logo.png" />
+      <h2>Ninja Store</h2>
+    </div>
+
+    <div id="body">
+
+      <div id="mensagem">
+        <p>Nossos produtos estão disponíveis no MercadoLivre, veja:</p>
+
+        <a href="http://lista.mercadolivre.com.br/_CustId_197993999" target="_blank">
+          <img id="logo-mercadolivre" src="images/mercadolivre.png" width="150px">
+        </a>
+
+      </div>
+    </div>
+
+
+    <div id="footer">
+
+      <ul>
+        <li>
+          <a href="mailto:ninjastoreml@gmail.com" target="_blank">
+            <img src="images/gmail.png">
+            ninjastoreml@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com/ninjastoreml" target="_blank">
+            <img src="images/facebook.png">
+            /ninjastoreml
+          </a>
+        </li>
+        <li>
+          忍者店
+        </li>
+      </ul>
+    </div> <!-- end footer -->
+
+  </div> <!--  end main -->
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-71396409-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
